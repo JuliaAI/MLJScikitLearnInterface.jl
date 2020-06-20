1c003161b74cebd2fbd31fda5cf7199ec73cf9ae
Okon Samuel <okonsamuel50@gmail.com>
2020-06-20 14:58:27 +0100

    Port clustering tests from MLJModels

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,13 +9,11 @@ const MB = MLJBase
 include("testutils.jl")
 include("macros.jl")
 
-println("linear-regressors");       include("models/linear-regressors.jl")
-println("linear-regressors-multi"); include("models/linear-regressors-multi.jl")
+println("\nlinear-regressors");       include("models/linear-regressors.jl")
+println("\nlinear-regressors-multi"); include("models/linear-regressors-multi.jl")
 
-println("linear-classifiers");      include("models/linear-classifiers.jl")
-println("discriminant-analysis");   include("models/discriminant-analysis.jl")
-println("gaussian-process");        include("models/gaussian-process.jl")
-println("ensemble");                include("models/ensemble.jl")
-
-
-println("extras"); include("extras.jl")
+println("\nlinear-classifiers");      include("models/linear-classifiers.jl")
+println("\ndiscriminant-analysis");   include("models/discriminant-analysis.jl")
+println("\ngaussian-process");        include("models/gaussian-process.jl")
+println("\nensemble");                include("models/ensemble.jl")
+println("\nclustering");              include("models/clustering.jl")
